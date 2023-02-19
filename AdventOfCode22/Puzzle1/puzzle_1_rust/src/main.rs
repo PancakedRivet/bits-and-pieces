4958b1a3149ec9c33930e97a1ea839d914d72b6e
Patrick <devanepatrick@gmail.com>
2023-02-19 21:32:50 +1300

    Added a Rust solution to Day 1.

---
+++ AdventOfCode22/Puzzle1/puzzle_1_rust/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+fn main() {
+    // Open the input file
+    let file_path = r"D:\Git\bits-and-pieces\AdventOfCode22\Puzzle1\input.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+	let mut calorie_value: u32 = 0;
+    let mut calorie_list: Vec<u32> = Vec::new();
+
+    for line in contents.lines() {
+        match line.parse::<u32>() {
+            Ok(n) => {
+                calorie_value += n;
+            },
+            _ => {
+                // If the line was not able to be parsed to an int, then it must be an empty line
+                calorie_list.push(calorie_value);
+                //Reset the calorie counter
+                calorie_value = 0;
+            },
+        }
+    }
+
+    // Sort Ascending
+    calorie_list.sort();
+    // Sort Descending (easier to slice)
+    calorie_list.reverse();
+    
+    // Part One
+    println!("Part One: Calories of highest elf: {:?}", &calorie_list[0]);
+
+    // Part Two
+    let sum: u32 = calorie_list[0..=2].iter().sum();
+    println!("Part Two: Calories of top 3 elves: {:?}", sum);
+}
