8959a657ec2d73140f2b50df905c3267f741ba53
Patrick <devanepatrick@gmail.com>
2023-02-20 18:01:14 +1300

    Minor tweaks and comments.

--- AdventOfCode22/Puzzle1/puzzle_1_rust/src/main.rs
+++ AdventOfCode22/Puzzle1/puzzle_1_rust/src/main.rs
@@ -5,20 +5,21 @@ fn main() {
     let file_path = r"D:\Git\bits-and-pieces\AdventOfCode22\Puzzle1\input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-	let mut calorie_value: u32 = 0;
+    let mut calorie_value: u32 = 0;
     let mut calorie_list: Vec<u32> = Vec::new();
 
     for line in contents.lines() {
         match line.parse::<u32>() {
             Ok(n) => {
+                // Add the value of the line to the current calorie value
                 calorie_value += n;
-            },
+            }
             _ => {
                 // If the line was not able to be parsed to an int, then it must be an empty line
                 calorie_list.push(calorie_value);
-                //Reset the calorie counter
+                // Reset the calorie counter
                 calorie_value = 0;
-            },
+            }
         }
     }
 
@@ -26,9 +27,9 @@ fn main() {
     calorie_list.sort();
     // Sort Descending (easier to slice)
     calorie_list.reverse();
-    
+
     // Part One
-    println!("Part One: Calories of highest elf: {:?}", &calorie_list[0]);
+    println!("Part One: Calories of highest elf: {:?}", calorie_list[0]);
 
     // Part Two
     let sum: u32 = calorie_list[0..=2].iter().sum();
