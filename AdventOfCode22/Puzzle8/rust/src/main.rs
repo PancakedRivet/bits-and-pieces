6b4e3662d6085c76fe164ba82139d7c0a291e1e8
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-03-31 19:41:10 +1300

    Part 1 complete.

--- AdventOfCode22/Puzzle8/rust/src/main.rs
+++ AdventOfCode22/Puzzle8/rust/src/main.rs
@@ -1,62 +1,75 @@
 use std::fs;
 
+// https://adventofcode.com/2022/day/8
+
 fn main() {
     // Open the input file
-    let file_path: &str = r"/Users/patrick/Code/bits-and-pieces/AdventOfCode22/Puzzle8/input2.txt";
+    let file_path: &str = r"/Users/patrick/Code/bits-and-pieces/AdventOfCode22/Puzzle8/input.txt";
     let contents: String = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let rev_contents: String = contents.clone().chars().rev().collect();
+
     let mut file_lines = contents.lines();
+    let mut rev_file_lines = rev_contents.lines();
+
+    let mut combined_visible_trees_vec = Vec::<Vec<bool>>::new();
+    let mut all_visible_trees_vec = Vec::<Vec<bool>>::new();
+    let mut rev_all_visible_trees_vec = Vec::<Vec<bool>>::new();
 
     // Get the first line as this is used to create the initial vector or tree heights as seen from the top
     let first_line = file_lines.next().unwrap();
-    println!("{:}", first_line);
+    let mut tree_height_vec_top: Vec<i32> = top_boundary_trees(first_line);
+    all_visible_trees_vec.push(vec![true; tree_height_vec_top.len()]);
+    combined_visible_trees_vec.push(vec![false; tree_height_vec_top.len()]);
 
-    let mut visibile_tree_count_total: i32 = 0;
-    let mut tree_height_vec_top: Vec<i32> = top_boundary_trees(&mut visibile_tree_count_total, first_line);
+    // Get the first line as this is used to create the initial vector or tree heights as seen from the top
+    let rev_first_line = rev_file_lines.next().unwrap();
+    let mut tree_height_vec_bottom: Vec<i32> = top_boundary_trees(rev_first_line);
+    rev_all_visible_trees_vec.push(vec![true; tree_height_vec_bottom.len()]);
 
     for line in file_lines {
-        println!("{:}", line);
+        let visibile_tree_vec = calculate_visible_trees(line, &mut tree_height_vec_top);
+        all_visible_trees_vec.push(visibile_tree_vec);
+        combined_visible_trees_vec.push(vec![false; tree_height_vec_top.len()]);
+    }
 
-        let mut visible_tree_count = 0;
+    for line in rev_file_lines {
+        let visibile_tree_vec = calculate_visible_trees(line, &mut tree_height_vec_bottom);
+        rev_all_visible_trees_vec.push(visibile_tree_vec);
+    }
 
-        let mut max_tree_height_left: i32 = -1;
+    println!("Dimensions: {:} x {:}", combined_visible_trees_vec.len(), combined_visible_trees_vec[0].len());
+    
+    for i in 0..all_visible_trees_vec.len() {
+        for j in 0..all_visible_trees_vec[0].len() {
+            let top_left_visible = all_visible_trees_vec[i][j];
+            // println!("top_left_visible: ({:},{:}) = {}", i, j, top_left_visible);
 
-        // let split_line_left = line.split("");
-        // let split_line_right = line.rsplit("");
-
-        let line_split: Vec<&str> = line.split("").collect();
-
-        // filter out the empty string vec elements (artifact of the split("") call)
-        let line_split_not_empty = line_split.iter().filter(|x| !x.is_empty()).copied();
-
-        // let line_vec: Vec<bool> = vec![false; line.len()];
-
-        for (i, tree_height) in line_split_not_empty.enumerate() {
-
-            let tree_height: i32 = tree_height.parse::<i32>().unwrap();
-            
-            if tree_height > max_tree_height_left { // Can the tree be seen from the left?
-                println!("{} > {} Left", tree_height, max_tree_height_left);
-                visible_tree_count += 1;
-                max_tree_height_left = tree_height;
-            } else if tree_height > tree_height_vec_top[i] { // Can the tree be seen from the top?
-                println!("{} > {} Top", tree_height, tree_height_vec_top[i]);
-                visible_tree_count += 1;
-                tree_height_vec_top[i] = tree_height;
-            } else {
-                println!("{}", tree_height);
+            let rev_i = all_visible_trees_vec.len() - i - 1;
+            let rev_j = all_visible_trees_vec[0].len() - j - 1;
+            let bottom_right_visible = rev_all_visible_trees_vec[rev_i][rev_j];
+            // println!("bottom_right_visible: ({:},{:}) = {}", rev_i, rev_j, bottom_right_visible);
+            if top_left_visible || bottom_right_visible {
+                combined_visible_trees_vec[i][j] = true;
             }
         }
+    }
 
-        println!("{} trees visible", visible_tree_count);
+    // println!("{:?}", all_visible_trees_vec);
+    // println!("{:?}", rev_all_visible_trees_vec);
+    // println!("{:?}", combined_visible_trees_vec);
 
-        visibile_tree_count_total += visible_tree_count
-    };
+    let mut visibile_tree_count_total: i32 = 0;
 
-    println!("There are {:} visible trees in total", visibile_tree_count_total)
+    for tree_line in combined_visible_trees_vec {
+        let total_true = tree_line.into_iter().filter(|b| *b).count();
+        visibile_tree_count_total += total_true as i32;
+    }
+
+    println!("There are {:} visible trees in total", visibile_tree_count_total);
 
 }
 
-fn top_boundary_trees(visibile_tree_count_total: &mut i32, first_line: &str) -> Vec<i32> {
+fn top_boundary_trees(first_line: &str) -> Vec<i32> {
     let first_line_split: Vec<&str> = first_line.split("").collect();
     // filter out the empty string vec elements (artifact of the split("") call)
     let first_line_split_not_empty = first_line_split.iter().filter(|x| !x.is_empty()).copied();
@@ -68,27 +81,35 @@ fn top_boundary_trees(visibile_tree_count_total: &mut i32, first_line: &str) -> 
         let tree_height: i32 = tree_height.parse::<i32>().unwrap();
 
         if tree_height > tree_height_vec_top[i] { // Can the tree be seen from the top?
-            println!("{} > {} Top", tree_height, tree_height_vec_top[i]);
-            *visibile_tree_count_total += 1;
             tree_height_vec_top[i] = tree_height;
         }
     }
 
-    println!("{} trees visible", visibile_tree_count_total);
-
     tree_height_vec_top
 }
 
-/* Ideas:
-- Create a vector of booleans (false) for tree visibility
-- split the string then collect it into a vector
-- iterate the vector (collection needed for iterating forwards and backwards)
-    - mark any visbile trees as true in the boolean vector (only needs to be visible form one side)
-- sum the number of "true" values in the vector to get the total visible trees for the line
+fn calculate_visible_trees(line: &str, tree_height_vec_top: &mut Vec<i32>) -> Vec<bool> {
+    let mut max_tree_height_left: i32 = -1;
 
-- TODO: do something for the columns (above and below)
-    - possibly have a vector of the highest trees encountered while iterating down the input list
-        - if the current column is taller, count it as visible and update it.
+    let mut tree_height_is_visible_vec = vec![false; tree_height_vec_top.len()];
 
-- include the trees on the outer perimeter in the calculation for total visible trees.
-*/
+    let line_split: Vec<&str> = line.split("").collect();
+    // filter out the empty string vec elements (artifact of the split("") call)
+    let line_split_not_empty = line_split.iter().filter(|x| !x.is_empty()).copied();
+
+    for (i, tree_height) in line_split_not_empty.enumerate() {
+
+        let tree_height: i32 = tree_height.parse::<i32>().unwrap();
+
+        if tree_height > tree_height_vec_top[i] { // Can the tree be seen from the top?
+            tree_height_vec_top[i] = tree_height;
+            tree_height_is_visible_vec[i] = true;
+        } 
+        if tree_height > max_tree_height_left { // Can the tree be seen from the left?
+            max_tree_height_left = tree_height;
+            tree_height_is_visible_vec[i] = true;
+        } 
+    }
+
+    tree_height_is_visible_vec
+}
