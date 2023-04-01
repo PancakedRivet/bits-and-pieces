751172d8168a43ee646c6665b893ae2c23e341a0
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-04-01 14:51:16 +1300

    Part 2 complete

--- AdventOfCode22/Puzzle10/rust/src/main.rs
+++ AdventOfCode22/Puzzle10/rust/src/main.rs
@@ -17,13 +17,36 @@ fn main() {
 
     let mut lines = contents.lines();
 
+    // Part 2:
+    let mut screen_line: String = "".to_string();
+
     loop {
 
+        // Part 2: "Rendering" to a line
+        {
+            // Determining what to print:
+            let sprite_range: i32 = ((current_cycle - 1) % 40) - current_register_value;
+            // println!("{}, {}, {}", current_cycle, current_cycle % 40, current_register_value);
+            if  sprite_range.abs() <= 1 {
+                screen_line = screen_line.to_owned() + "#";
+            } else {
+                screen_line = screen_line.to_owned() + ".";
+            }
+
+            // Print to the screen on every 40th cycle
+            if current_cycle % 40 == 0 {
+                println!("{}", screen_line);
+                screen_line = "".to_string();
+            }
+        }
+
+        // Part 1: 
+        
         // Check the signal strength every 40 cycles (after 20)
         if (current_cycle + 20) % 40 == 0 {
             let signal_strength = current_cycle * current_register_value;
             signal_strength_total += signal_strength;
-            println!("Cycle: {}, Register: {}, Signal Strength: {}, Total: {}", current_cycle, current_register_value, signal_strength, signal_strength_total);
+            // println!("Cycle: {}, Register: {}, Signal Strength: {}, Total: {}", current_cycle, current_register_value, signal_strength, signal_strength_total);
         }
 
         if !is_performing_op {
@@ -58,8 +81,5 @@ fn main() {
     }
 
     println!("Part 1: Signal strength sum: {}", signal_strength_total)
-
-    // Part 2: https://adventofcode.com/2022/day/10#part2
-
 
 }
